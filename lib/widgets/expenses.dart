ef3bcccfd0c191b83c7341e9ae9684c3c58db666
kipkoechke <chepkwonyke1@gmail.com>
2023-05-04 14:49:23 +0300

    Added modal bottom sheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -25,13 +26,22 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     ),
   ];
+
+  void _addExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ],
