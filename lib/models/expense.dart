4f8b7a63ecc1f67eb5bee201aeba6829d9025d6f
kipkoechke <chepkwonyke1@gmail.com>
2023-05-10 14:07:19 +0300

    Filtered list

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -16,13 +16,12 @@ const categoryIcons = {
 };
 
 class Expense {
-  Expense(
-      {required this.title,
-      required this.amount,
-      required this.date,
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
     required this.category,
-  })
-      : id = uuid.v4();
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
@@ -32,5 +31,28 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
